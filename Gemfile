2fef926719478308638797ff6af3bdfad9833133
tsubasa namatame <nyshk97@gmail.com>
2023-04-21 13:56:52 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in d0ne1s_hello_world.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/d0ne1s_hello_world.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "d0ne1s_hello_world/version"
+
+module D0ne1sHelloWorld
+  class Error < StandardError; end
+  # Your code goes here...
+end
