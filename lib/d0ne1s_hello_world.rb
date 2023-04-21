330eeb911249c9d551321fa8f4c8e39b0688ef2c
tsubasa namatame <nyshk97@gmail.com>
2023-04-21 14:15:55 +0900

    処理の中身を記述

--- lib/d0ne1s_hello_world.rb
+++ lib/d0ne1s_hello_world.rb
@@ -5,4 +5,7 @@ require_relative "d0ne1s_hello_world/version"
 module D0ne1sHelloWorld
   class Error < StandardError; end
   # Your code goes here...
+  def self.hello
+    "Hello World"
+  end
 end
